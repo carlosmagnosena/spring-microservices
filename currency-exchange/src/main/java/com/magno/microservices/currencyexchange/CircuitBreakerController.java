92cb2908b5d5f827098e049328294df5ff706b24
Carlos Magno <carlos@magno.com>
2022-08-16 18:08:07 +0200

    Added distributed tracing

--- currency-exchange/src/main/java/com/magno/microservices/currencyexchange/CircuitBreakerController.java
+++ currency-exchange/src/main/java/com/magno/microservices/currencyexchange/CircuitBreakerController.java
@@ -1,5 +1,34 @@
 package com.magno.microservices.currencyexchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+
+@RestController
 public class CircuitBreakerController {
 
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+	@GetMapping("/sample-api")
+	// @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+	// @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
+	// @RateLimiter(name = "default")
+	// 10s => 10000 calls to the sample api
+	@Bulkhead(name = "default")
+	public String sampleApi() {
+
+		logger.info("########" + "Sample API call received");
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy",
+//				String.class);
+//		return forEntity.getBody();
+
+		return "sample-api";
+	}
+
+	public String hardCodedResponse(Exception ex) {
+		return "Fallback Response";
+	}
 }
--- currency-exchange/src/main/java/com/magno/microservices/currencyexchange/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/magno/microservices/currencyexchange/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.magno.microservices.currencyexchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private Environment enviroment;
 
@@ -17,6 +21,7 @@ public class CurrencyExchangeController {
 
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		logger.info("retrieveExchangeValue called with {} to {} ", from, to);
 		CurrencyExchange currencyExchange = currencyRepository.findByFromAndTo(from, to);
 		if (currencyExchange == null) {
 			throw new RuntimeException("Unable to Find data for " + from + " and to: " + to);
